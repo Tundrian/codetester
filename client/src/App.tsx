bcc874cfda4fba11c963e287e4e9025c390eaf14
Tundrian <antoniosansotta@gmail.com>
2022-10-04 20:14:33 -0400

    added challenge2 component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Challenge1 from './components/strings/Challenge1';
+import Challenge2 from './components/strings/Challenge2';
 
 function App() {
   const categoryLinks: string[] = [
@@ -9,41 +10,39 @@ function App() {
   ]
   const stringLinks: { [key: string]: string[] } = {
     'Strings': [
-      'Challenge #1',
-      'Challenge #2'
-    ],
-    'Arrays': [
-      'Challenge #1'
+      'Challenge1',
+      'Challenge2'
     ]
   }
   const [activeCategory, setActiveCategory] = React.useState<string>('')
-
+  const [activeChallenge, setActiveChallenge] = React.useState<string>('')
+  // const [ActiveLink, setActiveLink] = React.useState<string>('')
 
   return (
     <>
-    <header>
-     <h1 className="text-center text-4xl text-cyan-500 py-5 w-full bg-gray-900">Blawblawlaw Blog Challenges</h1>
+      <header>
+        <h1 className="text-center text-4xl text-cyan-500 py-5 w-full bg-gray-900">Blawblawlaw Blog Challenges</h1>
 
-     <ul className="flex justify-start w-screen h-10 bg-slate-600 border-b border-b-slate-900">
-      {categoryLinks.map(link => (
-        <li className="w-fit h-full bg-slate-500 text-white border-r border-r-slate-900 border-r-2 hover:bg-slate-400">
-          <button className="w-full h-full align-middle block px-5" onClick={() => setActiveCategory(link)}>{link}</button>
-      </li>)
-      )}
-     </ul>
+        <ul className="flex justify-start w-screen h-10 bg-slate-600 border-b border-b-slate-900">
+          {categoryLinks.map(link => (
+            <li className="w-fit h-full bg-slate-500 text-white border-r border-r-slate-900 border-r-2 hover:bg-slate-400">
+              <button className="w-full h-full align-middle block px-5" onClick={() => setActiveCategory(link)}>{link}</button>
+          </li>)
+          )}
+        </ul>
 
-     <ul className="flex justify-start w-screen h-10 bg-slate-400">
-        {activeCategory && stringLinks[activeCategory].map((link: string) => (
-          <li className="w-fit h-full bg-slate-400 text-white border-r border-r-slate-900 border-r-2 hover:bg-slate-500">
-          <button className="w-full h-full align-middle block px-5">{link}</button>
-      </li>)
-        )}
-     </ul>
+        <ul className="flex justify-start w-screen h-10 bg-slate-400">
+            {activeCategory && stringLinks[activeCategory].map((link: string) => (
+              <li className="w-fit h-full bg-slate-400 text-white border-r border-r-slate-900 border-r-2 hover:bg-slate-500">
+              <button className="w-full h-full align-middle block px-5" onClick={() => setActiveChallenge(link)}>{link}</button>
+          </li>)
+            )}
+        </ul>
+    </header>
 
-  </header>
-
-  <Challenge1 />
-  </>
+    {activeChallenge === 'Challenge1' && <Challenge1 />}
+    {activeChallenge === 'Challenge2' && <Challenge2 />}
+  </> 
   );
 }
 
---
+++ client/src/components/strings/Challenge2.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Challenge2() {
+  const [input, setInput] = React.useState<string>("af)|Cz>:CGAHY1_2+(OHYYTM!2vka,&yAlWJ3.r&ZVs4F5&bEa<1r1_nlsR!eA-dOUmgwd2=CE7?ynnl+KQf8lW8u4/Nh1|D7SK2uYz-Y $YD)q.VmLD-&P(lL=:HDMA1!?_R#P,V3bKLqUp_t.X:_dM<Mr9zQ~fKJOBDF&Qjen=d ZAR8ze5g-gj@aZ/I:M:!.IBXLq@vN&8L7oH, /0RFtH+.7%eXtM/mxV7*%0Hj.?FSm.Kh8=@jlUJ_S.ApQzye-xB7ZVA2AwEaxTa_snPX?>D5th&Ag/9)7%#uMN=J<h!#prB=:CI;U_.T5*#rb)9q<ht~W<BOmQlmy.<8EqIlR(Gh~~$zmW;Y$7dn-$bW1jf%PL),krpzE LQwsG;EQ;l:Z;+6#.3)KDA+s#k#YMmUhm% 2KB)GqR U@4B>+A9Vh62@:&SNKVas5%&N6Oz!s/%7(Pd1xWy#Kv>uqyX=VEyHbe-64|ex^& 2W(gD=>FT*?tMZO$(+B8uTj,DU2~QST(B|xls=9kzN|:4hXt~:R4|nir,6RN8fg~3!3!^!yWZ)bE@SI.zV5Z%V9Qf_^ 4az$?wqtWrRu+1aS9<%I=!xW.:ps f).($Qz9s;8:uxVqJN9K$42NG(0=j)wZ&U1oB?v^7WVP6QE6-?L;^aglPd5dHO(L2TT5+$NeK-P;js-UAiWCI7>Yr&8|md-X)U=%IpuY)9iymv~ _S^ ,&0|(22&(mIcux_4a196w^FN78kz2kX1k&Psc53d ctbl?Eek!kX7Ii.QR~M11<T!,w^xHVn4^Q=HH4#)=AevEek!ux_4a196w^FN7Eek!8kz2kX1k&Psc53d ctbl?kX7Ii.QR~M11<T!,w^xHVn4^Q=HH4#)=AEek!Eek!cvEek!MABf|bEek!dPLpPhh=>%UH<%/^H;8gSAkhmUCu:K?*jzEek!LYC/:E*RU=|s7jWDf8z7-zFqS<Ntoav?m8<Q(1ur3Dpd)(XjY1pE)but>eG@QfEek!$Wx8NRZgmQ5t2 kJd,?_R@%muEJBTdQ0-%mHHW20i8wqMK~Co^w?34ag9idsTeXgfqy4IEek!z7Q?Q)nG~5@~ieY_B=6! sVk~=Ts3T>j/ZAS)AKX3zOocGL icK#-x0Eek!yE+2*fz.4&%<>:RHx/B+IFRG*AK1Hn*PQZpEek!v;ZUzWcJC%lym^:+;GcC!qt@nS5SQEek!ikESbYI#-A.Sv .ksat s'worrEek!Eek!omot Eek!rof rebmeEek!merEek! Eek!ot deeEek!n uoy tahw si enEek!o tsrif siht sedisEek!eb gnirtsbus hcae ni retEek!carahc tsrifEek! eEek!Eek!hT .'V' *esaEek!crewEek!ol* nEek!Eek!o pu Eek!gnirts Eek!siEek!ht tilpEek!S"
+  )
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
+  const decode = (msg: string) => {
+    return msg.slice(msg.indexOf('c')+1).replace(/Eek!/g, '').split('').reverse().join('').split('v')
+  }
+
+
+  return (
+    <section className="w-screen flex justify-center flex-nowrap align-middle flex-col">
+      <h2 className="text-center text-3xl text-slate-700 my-5 w-full">Challenge #2: A slight detour on the road to Friday</h2>
+      <form action="#" className="flex justify-center align-middle bg-slate-400 w-full">
+        <div className="flex justify-center w-full flex-wrap">
+          <label htmlFor="strInput" className="text-gray-800 text-2xl pt-2 w-full text-center">Encrypted message</label>
+          <input name="strInput"  id="strInput" className="border border-slate-600 w-3/4 h-10 mb-5 text-center" type="text" value={input} onChange={handleChange} />
+        </div>
+      </form>
+      {decode(input) && decode(input).map(x => (
+        <p className="w-full h-10 bg-green-700 text-white text-center py-2"><span className="font-bold text-gray-200 text-lg">Output: </span>{x}</p>
+      ))}
+      {/* <p className="w-full h-10 bg-green-700 text-white text-center py-2"><span className="font-bold text-gray-200 text-lg">Output: </span>{decode(input)}</p> */}
+    </section>
+  )
+}
+export default Challenge2
