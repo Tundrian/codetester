efbe1e3a11e88ecdecb6d23bb4e68c5808d0c20c
Tundrian <antoniosansotta@gmail.com>
2022-10-08 16:32:48 -0400

    added updated descriptions to challenges

--- client/src/components/strings/Challenge7.tsx
+++ client/src/components/strings/Challenge7.tsx
@@ -5,18 +5,21 @@ function Challenge7() {
     "Thies its H alltowe!en!? Th#is Tis GHalolowmeen$!"
   );
 
-  const [num, setNum] = React.useState<number>(4)
+  const [num, setNum] = React.useState<number>(4);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
   const handleNumChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNum(+e.target.value)
-  }
+    setNum(+e.target.value);
+  };
 
   const decode = (msg: string) => {
-    return msg.split('').filter((x,i) => (i + 1) % num !== 0 ).join('');
+    return msg
+      .split("")
+      .filter((x, i) => (i + 1) % num !== 0)
+      .join("");
   };
 
   return (
@@ -26,7 +29,7 @@ function Challenge7() {
           Challenge #7
         </h2>
         <p className="text-center text-2xl text-blue-600 mb-5">
-        Weekly Challenge
+          Weekly Challenge
         </p>
 
         <div className="accordion mb-5" id="accordionExample">
@@ -65,165 +68,143 @@ function Challenge7() {
             >
               <div className="accordion-body py-4 px-5">
                 <p>
-                  So, I may have bitten off a bit more than I can chew this
-                  week. Will you help me out?{" "}
+                  Oooh, yeah! Here it is, the final function you'll need in your
+                  string-decoding arsenal this week. Today's challenge might not
+                  be too trying after all the others, but you never know!{" "}
                 </p>
                 <br />
                 <p>
-                  My friend has invented a 'Best Cat-Themed Pun of a TV Show
-                  Title Ever' award and I promised I'd help her with the
-                  submissions. The problem is, I didn't realize just how much
-                  help she would need whittling down the submissions to the
-                  finalists. Between work, family, and Huntober, I just don't
-                  have time to get the final list over to her. Can you help?
+                  Today you'll need to{" "}
+                  <strong>
+                    create a function that swaps each letter of the alphabet for
+                    its opposite
+                  </strong>
+                  . A letter's opposite is one that, if the alphabet were
+                  flipped Z-A, would be the same number of letters in, and which
+                  would also have the opposite case.
                 </p>
                 <br />
 
-                <p>Here is a comma-separated list of the submissions:</p>
-                <br />
-
-                <pre className="overflow-x-auto bg-slate-800 text-yellow-300 text-sm py-5 px-3">
+                <p>Examples:</p>
+                <pre  className="overflow-x-auto bg-slate-800 text-yellow-300 text-sm py-5 px-3">
                   <code>
-                    <span>
-                      "The Pawshank Redemption,Caturday Night Live,Only
-                      Meworders in the Building,I Love Mewcy, Mewsummer
-                      Meowders,The Golden Purrs, Purrlandia ,Meowpardy,
-                      Meowstery Science Theater: Purrthousand, Amewican Idol,Dog
-                      City,Doctor Mew , The Meowing Fed,Mew Peter,The Vicar of
-                      Dogley, Kittens,Meownton Abbey,Pets and the Kitty,Dogis
-                      and Bonehead,Pawlty Meowers ,The Meowpet Show,Barkos,The
-                      Catbert Purrport,The Pawffice,The Dogford Files,
-                      Battlestar Catlactica,Catlumbo,SpongeDog Squarepants,NYPD
-                      Mew ,Fluffy the Meowpire Purrer,The Inbemewners,Meowder
-                      She Wrote,Paw &amp; Order,30 Dog, Pawvatar: The Last
-                      Meowbender,The Pawnight Show,Arrested
-                      Dogvelopment,Furiends,Mewie,Curb Your Dogthusiasm,Teenage
-                      Mewtant Ninja Turtles,Phineas and Purrb,Paw Trek, Paw
-                      Trek: The Next Mewination, Twin Mewks, *C*A*T*S*,DogTales,
-                      Game of Bones, House of the Meowgon,The Purrlight
-                      Zone,Breaking Bone,The Meowre,The Dogpranos,The Rings of
-                      Meower, The KIT Crowd,Strangepaw Things ,Catman: The
-                      Animeowted Series,Meowter Call Saul,Mewgerton
-                      ,Obark,Mewphoria,La Casa de Pawpel,Rick &amp;
-                      Meowty,Amewican Purror Story,
-                      Mewcifer,PawndaVision,Dogxter,The Meowndalorian, Dog
-                      Lasso,Bark,Meowdern Pawmily , Meowtlander,Bone
-                      Mirror,Barks and Recreation,How to Get Away with
-                      Meowder,Boneland ,Meowther Ted,Mewtopia,Mewey,The Mewkie
-                      Meowse Doghouse,Mewster Rogers' Neighborhood"
+                    <span className="hljs-string">'A'</span>{" "}
+                    <span className="hljs-comment">
+                      // first letter of the alphabet, uppercase
+                    </span>
+                    <span className="hljs-string">'z'</span>{" "}
+                    <span className="hljs-comment">
+                      // last letter of the alphabet, lowercase
+                    </span>
+                    <span className="hljs-string">'p'</span>{" "}
+                    <span className="hljs-comment">
+                      // 16th letter of the alphabet, lowercase
+                    </span>
+                    <span className="hljs-string">'K'</span>{" "}
+                    <span className="hljs-comment">
+                      // 16th letter from *end* of alphabet, uppercase
+                    </span>
+                    <span className="hljs-string">
+                      'vCZNKOV: 0 MFNYVI LI KFMXGFZGRLM XSZMTVH'
+                    </span>
+                    <span className="hljs-comment">
+                      // function replaces letters with opposites
+                    </span>
+                    <span className="hljs-string">
+                      'Example: 0 number or punctuation changes'
                     </span>
                   </code>
                 </pre>
                 <br />
 
                 <p>
-                  There are some very strict rules that the pun submissions had
-                  to follow. I need to know which of these cat puns made the
-                  cut. I promise tomorrow we'll get back to decoding our secret
-                  message, and coincidentally, I'm pretty sure{" "}
-                  <strong>
-                    the number of these puns that met the criteria
-                  </strong>{" "}
-                  will match a key sequence value we'll need tomorrow. Isn't
-                  that lucky?
+                  <em>
+                    Do me a favor and try to solve it a couple of different ways
+                    to see which you preferred and why
+                  </em>
+                  .
                 </p>
                 <br />
 
-                <h5 id="heading-here-are-the-rules-these-submissions-had-to-follow">
-                  Here are the rules these submissions had to follow:
-                </h5>
-                <br />
-
-                <ul>
-                  <li>
-                    No empty spaces at the start or end of the submission{" "}
-                    <em>
-                      (my friend can't stand when people don't follow
-                      directions)
-                    </em>
-                  </li>
-                  <br />
-
-                  <li>
-                    Cannot contain 'dog', 'bark', or 'bone' in any combination
-                    of upper or lower case{" "}
-                    <em>(canine trolling can't be stopped)</em>
-                  </li>
-                  <br />
-
-                  <li>
-                    Total length of pun cannot be a multiple of 5{" "}
-                    <em>(this includes spaces and punctuation)</em>
-                  </li>
-                  <br />
-
-                  <li>
-                    The sum of the charCodes of the 1st and last characters must
-                    be odd{" "}
-                    <em>
-                      (my friend's a bit of a character, but she's great once
-                      you get to know her)
-                    </em>
-                  </li>
-                  <br />
-
-                  <li>
-                    Character directly after the middle of the string may not be
-                    'e'{" "}
-                    <em>
-                      (For example the character to check in even-length string{" "}
-                      <code>center</code> is <code>t</code>, for odd an example
-                      would be <code>weird</code> -&gt; 'r')
-                    </em>
-                  </li>
-                  <br />
-
-                  <li>
-                    Must have an even number of lowercase letters (do not count
-                    punctuation or spaces as letters)
-                  </li>
-                  <br />
-
-                  <li>
-                    Must have at least 2 capital letters{" "}
-                    <em>
-                      (honestly, I'm a little worried these requirements are too
-                      strict)
-                    </em>
-                  </li>
-                  <br />
-
-                  <li>
-                    Must not contain a capital 'S'{" "}
-                    <em>
-                      (definitely too strict. What does S even have to do with
-                      cat puns?)
-                    </em>
-                  </li>
-                </ul>
+                <h2 id="heading-put-it-all-together">🧩 Put it all Together</h2>
                 <br />
 
                 <p>
-                  Send me a DM of the puns that made the cut{" "}
-                  <a target="_blank" href="https://twitter.com/huntoberTweets">
-                    on Twitter
-                  </a>
-                  , and go ahead and save the final count to use tomorrow.
-                  Thanks!
+                  Finally! We have every function needed to decode the
+                  gobbledygook I shared a week ago! Some of you already figured
+                  it out yesterday (bravo!), but let's pretend you didn't.
                 </p>
                 <br />
 
-                <h3 id="heading-it-still-looks-like-gibberish">
-                  🕵️ It still looks like Gibberish
+                <p>
+                  <strong>Please use the daily functions,</strong>{" "}
+                  <strong>
+                    <em>in order,</em>
+                  </strong>{" "}
+                  <strong>to decode the week's secret message</strong>.{" "}
+                  <strong>
+                    Then, when you feel good about your result, go ahead and
+                    tweet the string with no explanation and #huntober2022, you
+                    earned it.
+                  </strong>
+                </p>
+                <br />
+
+                <pre  className="overflow-x-auto bg-slate-800 text-yellow-300 text-sm py-5 px-3">
+                  <code>
+                    <span className="hljs-keyword">const</span> encryptedMsg ={" "}
+                    <span className="hljs-string">
+                      "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"
+                    </span>
+                  </code>
+                </pre>
+                <br />
+
+                <p>Two things to note:</p>
+
+                <br />
+                <ol>
+                  <li>
+                    It's very important that the functions are called in the
+                    order they were given (e.g. Day 1 first, Day 7 last), as any
+                    deviation could throw things far out of whack
+                  </li>
+                  <li>
+                    Day 2 and Day 5 weren't designed to be part of this overall
+                    decoder. The <strong>answers</strong> you got on those days
+                    should be used as inputs for Days 3 &amp; 6 respectively
+                  </li>
+                </ol>
+                <h2 id="heading-what-now">👏 What now?</h2>
+                <br />
+                <p>
+                  Please feel free to share your answers at each step, your
+                  code, feelings about the challenges, etc. as you please! I
+                  only ask that any spoilers have a little warning in case
+                  anyone wants to come in fresh later.
+                </p>
+                <br />
+                <p>
+                  I'm torn on if I should share my code or not, as I went mostly
+                  brute force with this and the code is that of the baddie that
+                  I am.{" "}
+                  <em>
+                    Let me know in the comments what you think I should do.
+                  </em>
+                </p>
+                <br />
+                <p>
+                  If you really want to go wild, see if you can reverse engineer
+                  an <em>encoding</em> sequence. A solution that fits any given
+                  string, key characters, and decoy sequence might not be as
+                  easy as it appears...
+                </p>
+                <br />
+                <h3 id="heading-come-back-tomorrow-for-week-2-day-1">
+                  Come back tomorrow for Week 2 - Day 1!
                 </h3>
                 <br />
-
-                <p>
-                  If you've started trying to decode this week's message you may
-                  be wondering if you're on the right track. Chances are if it
-                  still looks like someone mashed the keyboard, you're doing
-                  A-OK! Feel free to compare notes!
-                </p>
+                <p>It's gonna be fun 🤓 </p>
                 <br />
 
                 <div className="embed-wrapper">
@@ -232,7 +213,7 @@ function Challenge7() {
                     <div className="loadingRow"></div>
                   </div>
                   <a
-                    className="embed-card"
+                    style={{color: "blue", textDecoration:"underline"}}
                     href="https://twitter.com/wo1vin/status/1577413631691853824"
                   >
                     https://twitter.com/wo1vin/status/1577413631691853824
